7f43e4de51e7b91d15aab90f9d57925351768dcd
tetsutooo <john.tetsuto0@gmail.com>
2024-10-21 00:16:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
             }]
         },
         options: {
-            responsive: true,
+            responsive: false,
             maintainAspectRatio: false,
             scales: {
                 x: {
