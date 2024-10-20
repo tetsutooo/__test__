2dd6cae2cd291d2ed4953ba1f79154360d4548de
tetsutooo <john.tetsuto0@gmail.com>
2024-10-20 14:32:51 +0900

    Add files via upload

---
+++ pkg/wasm_math_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_heatmapdata_free(a: number, b: number): void;
+export function heatmapdata_new(a: number, b: number): number;
+export function heatmapdata_set_value(a: number, b: number, c: number, d: number): void;
+export function heatmapdata_get_data(a: number, b: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
