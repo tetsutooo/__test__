3f973b8fe2a1b585c0bf2d5fdf15056aba00cf32
tetsutooo <john.tetsuto0@gmail.com>
2024-10-25 22:29:46 +0900

    Add files via upload

---
+++ pkg/wasm_math_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_singledefectmodel_free(a: number, b: number): void;
+export function singledefectmodel_new(): number;
+export function singledefectmodel_width(a: number): number;
+export function singledefectmodel_update(a: number): void;
+export function singledefectmodel_get_configuration(a: number, b: number): void;
+export function singledefectmodel_get_size_distribution(a: number, b: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
